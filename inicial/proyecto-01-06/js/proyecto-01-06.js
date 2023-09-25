5c45790056cb1592ebbe7c203701fa6ac5630e3d
Iván García <114983932+SINESCUD0@users.noreply.github.com>
2023-09-25 09:09:44 +0200

    Update proyecto-01-06.js

--- inicial/proyecto-01-06/js/proyecto-01-06.js
+++ inicial/proyecto-01-06/js/proyecto-01-06.js
@@ -1,1 +1,11 @@
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("texto").style.marginLeft = "210px";
+  document.getElementById("titulo").style.marginLeft = "210px";
+}
 
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("texto").style.marginLeft = "0px";
+  document.getElementById("titulo").style.marginLeft = "0px";
+}
